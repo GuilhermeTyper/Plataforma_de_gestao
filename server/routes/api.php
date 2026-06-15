eff3d3559bf010014f7f1a6f185261f8ebca8fc4
guilherme <270210.gui@gmail.com>
2026-06-14 23:59:48 -0300

    Remove unused user route and clean up API routes for user, task, and event creation

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\EventController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/cadastro', [UserController::class, 'store']);
+Route::post('/tarefas', [TaskController::class, 'store']);
+Route::post('/eventos', [EventController::class, 'store']);
+
